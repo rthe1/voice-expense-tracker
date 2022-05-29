2bb8c136e7ff2d8c9e75b59859bf2466b38658c9
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-05-29 05:31:28 -0400

    Charts working and custom hook implemented

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react'
 import { TextField, Button,  Typography, Grid, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 import { ExpenseTrackerContext } from '../../../context/context';
 import {v4 as uuidv4} from 'uuid'
+import formatDate from '../../../utils/formatDate'
 
 import useStyles from './styles'
 
@@ -11,7 +12,7 @@ const initialState =  {
   amount : '',
   category: '',
   type: 'Income',
-  date: new Date(),
+  date: formatDate(new Date()),
 
 }
 
@@ -61,7 +62,7 @@ const selectedCategories = formData.type === "Income" ? incomeCategories : expen
         <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({...formData, amount: e.target.value })} />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({...formData, date: e.target.value })}/>
+        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({...formData, date: formatDate(e.target.value) })}/>
       </Grid>
       <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>CREATE</Button>
     </Grid>
